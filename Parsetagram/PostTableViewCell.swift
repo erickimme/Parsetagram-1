7a863c33787a52291efb5baf35d6ae54928ea0dd
ryujimano <ryujimano@gmail.com>
2017-03-11 23:51:07 -0500

    update layout

---
+++ Parsetagram/PostTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PostTableViewCell.swift
+//  Parsetagram
+//
+//  Created by Ryuji Mano on 3/11/17.
+//  Copyright © 2017 Ryuji Mano. All rights reserved.
+//
+
+import UIKit
+
+class PostTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
