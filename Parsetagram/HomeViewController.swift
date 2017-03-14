efc30dc077cef130e980f4f88076d1cb8aa4b2d5
ryujimano <ryujimano@gmail.com>
2017-03-13 23:41:29 -0400

    add signup/login functionality

--- Parsetagram/HomeViewController.swift
+++ Parsetagram/HomeViewController.swift
@@ -7,9 +7,12 @@
 //
 
 import UIKit
+import Parse
 
 class HomeViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
--- Parsetagram/LoginViewController.swift
+++ Parsetagram/LoginViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Parse
 
 class LoginViewController: UIViewController {
     @IBOutlet weak var usernameField: UITextField!
@@ -24,20 +25,55 @@ class LoginViewController: UIViewController {
     }
     
     @IBAction func loginTapped(_ sender: Any) {
+        PFUser.logInWithUsername(inBackground: usernameField.text!, password: passwordField.text!) { (user: PFUser?, error: Error?) in
+            if user != nil {
+                performSegue(withIdentifier: "loginSegue", sender: nil)
+            }
+            else {
+                let alert = UIAlertController(title: "An Error Occurred", message: error?.localizedDescription, preferredStyle: .alert)
+                let action = UIAlertAction(title: "Ok", style: .default, handler: nil)
+                
+                alert.addAction(action)
+                
+                self.show(alert, sender: nil)
+            }
+        }
     }
     
     @IBAction func signUpTapped(_ sender: Any) {
+        let newUser = PFUser()
+        
+        newUser.username = usernameField.text
+        newUser.password = passwordField.text
+        
+        newUser.signUpInBackground { (success: Bool, error: Error?) in
+            if success {
+                let alert = UIAlertController(title: "Sign Up Successful", message: "You have been successfully signed up. Please login to continue.", preferredStyle: .alert)
+                let action = UIAlertAction(title: "Ok", style: .default, handler: nil)
+                
+                alert.addAction(action)
+                
+                self.show(alert, sender: nil)
+            }
+            else {
+                let alert = UIAlertController(title: "An Error Occurred", message: error?.localizedDescription, preferredStyle: .alert)
+                let action = UIAlertAction(title: "Ok", style: .default, handler: nil)
+                
+                alert.addAction(action)
+                
+                self.show(alert, sender: nil)
+            }
+        }
     }
     
-
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        let destination = segue.destination
+        
     }
-    */
+    
 
 }
