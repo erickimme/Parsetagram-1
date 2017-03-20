7afd1449fbda7ec917aad9b2da0d0c6639697429
ryujimano <ryujimano@gmail.com>
2017-03-19 22:31:02 -0400

    update home cells

--- Parsetagram/HomeViewController.swift
+++ Parsetagram/HomeViewController.swift
@@ -12,7 +12,7 @@ import Parse
 class HomeViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     @IBOutlet weak var tableView: UITableView!
 
-    var posts: [PFObject]?
+    var posts: [Post]?
     var images: [UIImage]?
     
     override func viewWillAppear(_ animated: Bool) {
@@ -54,22 +54,27 @@ class HomeViewController: UIViewController, UITableViewDelegate, UITableViewData
         
         let post = posts?[(posts?.count)! - indexPath.row - 1]
         
-        let user = post?["author"] as? PFUser
-        try! user?.fetchIfNeeded()
-        if let username = user?.username {
-            cell.usernameLabel.text = username
-        }
-        cell.captionLabel.text = post?["caption"] as? String
-        if let file = post?["media"] as? PFFile {
-            file.getDataInBackground(block: { (data: Data?, error: Error?) in
-                if (error != nil) {
-                    cell.postView.image = #imageLiteral(resourceName: "iconmonstr-picture-17-240")
-                }
-                else {
-                    cell.postView.image = UIImage(data: data!)
-                }
-            })
-        }
+//        let user = post?["author"] as? PFUser
+//        try! user?.fetchIfNeeded()
+//        if let username = user?.username {
+//            cell.usernameLabel.text = username
+//        }
+//        cell.captionLabel.text = post?["caption"] as? String
+//        if let file = post?["media"] as? PFFile {
+//            file.getDataInBackground(block: { (data: Data?, error: Error?) in
+//                if (error != nil) {
+//                    cell.postView.image = #imageLiteral(resourceName: "iconmonstr-picture-17-240")
+//                }
+//                else {
+//                    cell.postView.image = UIImage(data: data!)
+//                }
+//            })
+//        }
+//
+        
+        cell.usernameLabel.text = post?.username
+        cell.captionLabel.text = post?.caption
+        cell.postView.image = post?.post
         
         return cell
         
@@ -78,7 +83,11 @@ class HomeViewController: UIViewController, UITableViewDelegate, UITableViewData
     
     func getImages() {
         Post.getImages(success: { (posts: [PFObject]) in
-            self.posts = posts
+            var arr: [Post] = []
+            for post in posts {
+                arr.append(Post(post))
+            }
+            self.posts = arr
             self.tableView.reloadData()
         }) { (error: Error) in
             let alert = UIAlertController(title: "An Error Occurred", message: error.localizedDescription, preferredStyle: .alert)
